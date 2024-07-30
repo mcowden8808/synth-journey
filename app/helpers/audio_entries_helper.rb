a0cd358d8bd3d7b71c70ed881e3a9acce9ffdb2f
mcowden8808 <mcowden808@yahoo.com>
2024-07-30 16:49:31 -0400

    generate controller AudioEntries

---
+++ app/helpers/audio_entries_helper.rb
@@ -0,0 +1,2 @@
+module AudioEntriesHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'audio_entries/index'
+  get 'audio_entries/show'
+  get 'audio_entries/new'
+  get 'audio_entries/create'
+  get 'audio_entries/edit'
+  get 'audio_entries/update'
+  get 'audio_entries/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.

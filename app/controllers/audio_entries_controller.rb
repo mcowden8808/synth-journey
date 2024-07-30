8093028d3b2c2ecc7fa219d3bc351b14b5a69e08
mcowden8808 <mcowden808@yahoo.com>
2024-07-30 17:51:23 -0400

    add controller scaffolding, audio type validation

--- app/controllers/audio_entries_controller.rb
+++ app/controllers/audio_entries_controller.rb
@@ -1,22 +1,48 @@
 class AudioEntriesController < ApplicationController
   def index
+    @audio_entries = AudioEntry.all
   end
 
   def show
   end
 
   def new
+    @audio_entry = AudioEntry.new
   end
 
   def create
+    @audio_entry = AudioEntry.new(audio_entry_params)
+
+    if @audio_entry.save
+      redirect_to @audio_entry, notice: 'Audio entry was successfully created.'
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @audio_entry.update(audio_entry_params)
+      redirect_to @audio_entry, notice: 'Audio entry was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @audio_entry.destroy
+    redirect_to audio_entries_url, notice: 'Audio entry was successfully destroyed.'
+  end
+
+  private
+
+  def set_audio_entry
+    @audio_entry = AudioEntry.find(params[:id])
+  end
+
+  def audio_entry_params
+    params.require(:audio_entry).permit(:title, :description, :audio_file)
   end
 end
--- app/models/audio_entry.rb
+++ app/models/audio_entry.rb
@@ -1,2 +1,19 @@
 class AudioEntry < ApplicationRecord
+  has_one_attached :audio_file
+
+  validates :audio_file, presence: true
+  validate :correct_audio_file_type
+
+  private
+
+  def correct_audio_file_type
+    if audio_file.attached? && !valid_audio_type?
+      errors.add(:audio_file, 'must be an MP3 or WAV file')
+    end
+  end
+
+  def valid_audio_type?
+    audio_file.content_type.in?(%w(audio/mpeg audio/x-wav))
+  end
+
 end
